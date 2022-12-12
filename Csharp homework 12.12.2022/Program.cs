2e5601b7f2462f4747069feff571be40775a7422
Alakbar Hasanov <alakbarbh@code.edu.az>
2022-12-13 02:09:52 +0400

    C# homework 12.12.2022

---
+++ Csharp homework 12.12.2022/Program.cs
@@ -0,0 +1,120 @@
+﻿//static void GetRef(int n);
+
+
+
+//int n = 10;
+
+//GetRef(ref n);
+
+
+//Console.WriteLine(n);
+
+
+
+//static void GetRef(ref int n)
+//{
+
+//    n = 100;
+//        Console.WriteLine(n);
+//}
+
+
+
+
+
+//int n;
+
+//GetRef(out n);
+
+//Console.WriteLine(n);
+
+//static void GetRef(out int n) 
+//{
+//    n= 100;
+//    Console.WriteLine(n);
+//}
+
+
+
+
+
+
+//string address = "Bayil";
+
+//Console.WriteLine(address.StartsWith("B"));
+
+
+
+
+
+
+//string address = "Bayil";
+//Console.WriteLine(address.EndsWith("i"))
+
+
+
+
+//string address = "Bayil";
+//Console.WriteLine(address.Contains("i"));
+
+
+
+
+
+//string address = "Baki,Sheki,Qazax";
+
+//var res = address.Split("");
+
+//foreach (var item in res) ;
+//{
+//    Console.WriteLine(address);
+//}
+
+
+
+
+
+//string name = "ALAKBAR";
+
+//Console.WriteLine(name.ToLower("a"));
+
+
+
+
+//string name = "alakbar";
+
+//Console.WriteLine(name.ToUpper("a"));
+
+
+
+
+//string name = "Alakbar";
+
+//Console.WriteLine(name.Substring(0));
+
+
+
+
+
+
+
+
+
+
+
+
+//GetReverse("Alakbar");
+
+
+//static void GetReverse(string name)
+//{
+//    string res = " ";
+
+//	for (int i = name.Length - 1; i >= 0; i--)
+//	{
+//		res += name[i];
+
+//	}
+
+//	Console.WriteLine(res);
+//}
